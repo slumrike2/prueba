32dc9a64406060c5e71f67cc0104ece233221a48
Silva Bouchard, Jesus Alejandro <jasilva.22@est.ucab.edu.ve>
2024-01-23 15:53:25 -0400

    ppipipipip

---
+++ PruebaGitHub/Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+
+
+
+
+}
